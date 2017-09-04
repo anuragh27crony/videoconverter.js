79643cf7f38ba44553c06ca566e6142880a0c0a7
AnuragMala <anurag.mala@4cinsights.com>
2017-09-04 17:25:12 +0200

    fixed issue

--- build/build_custom.sh
+++ build/build_custom.sh
@@ -8,7 +8,7 @@ mkdir -p dist
 cd zlib
 make clean
 emconfigure ./configure --prefix=$(pwd)/../dist --64
-emmake make
+emmake make	
 emmake make install
 cd ..
 
@@ -20,7 +20,7 @@ make clean
 emconfigure ./configure --cc="emcc" --prefix=$(pwd)/../dist --enable-cross-compile --target-os=none --arch=x86_32 --cpu=generic \
     --disable-ffplay --disable-ffprobe --disable-ffserver --disable-asm --disable-doc --disable-devices --disable-pthreads --disable-w32threads --disable-network \
     --disable-asm --disable-fast-unaligned --disable-pthreads --disable-w32threads --disable-os2threads --disable-debug --disable-stripping --disable-all --enable-ffmpeg --enable-avcodec \
-    --enable-avformat --enable-avutil --enable-swresample --enable-swscale --enable-avfilter --disable-network --disable-d3d11va --disable-dxva2 --disable-vaapi --disable-vda --disable-vdpau
+    --enable-avformat --enable-avutil --enable-swresample --enable-swscale --enable-avfilter --disable-network --disable-dxva2 --disable-vaapi --disable-vda --disable-vdpau
     --enable-avcodec  --enable-avformat  --enable-avutil  --enable-swresample  --enable-swscale  --enable-avfilter --disable-network  --disable-d3d11va  --disable-dxva2 \
     --disable-vaapi  --disable-vda  --disable-vdpau  --enable-decoder=vp8  --enable-decoder=vp9  --enable-decoder=theora  --enable-decoder=mpeg2video  --enable-decoder=mpeg4 \
     --enable-decoder=h264  --enable-decoder=hevc  --enable-decoder=png  --enable-decoder=mjpeg  --enable-decoder=vorbis  --enable-decoder=opus  --enable-decoder=mp3 \
