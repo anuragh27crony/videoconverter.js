16f9a3f67bba34c0c25b0230a6df5457d4b6cd82
Anuragh27crony <Anuragh27crony@gmail.com>
2017-09-17 11:19:47 +0200

    updated sequence of flags

--- build/build_custom.sh
+++ build/build_custom.sh
@@ -18,18 +18,19 @@ cd ffmpeg
 
 make clean
 emconfigure ./configure --cc="emcc" --prefix=$(pwd)/../dist --enable-cross-compile --target-os=none --arch=x86_32 --cpu=generic \
-    --disable-ffplay --disable-ffprobe --disable-ffserver --disable-asm --disable-doc --disable-devices --disable-pthreads --disable-w32threads --disable-network \
-    --disable-asm --disable-fast-unaligned --disable-pthreads --disable-w32threads --disable-os2threads --disable-debug --disable-stripping --disable-all --enable-ffmpeg --enable-avcodec \
-    --enable-avformat --enable-avutil --enable-swresample --enable-swscale --enable-avfilter --disable-network --disable-dxva2 --disable-vaapi --disable-vda --disable-vdpau
-    --enable-avcodec  --enable-avformat  --enable-avutil  --enable-swresample  --enable-swscale  --enable-avfilter --disable-network  --disable-d3d11va  --disable-dxva2 \
-    --disable-vaapi  --disable-vda  --disable-vdpau  --enable-decoder=vp8  --enable-decoder=vp9  --enable-decoder=theora  --enable-decoder=mpeg2video  --enable-decoder=mpeg4 \
-    --enable-decoder=h264  --enable-decoder=hevc  --enable-decoder=png  --enable-decoder=mjpeg  --enable-decoder=vorbis  --enable-decoder=opus  --enable-decoder=mp3 \
-    --enable-decoder=ac3  --enable-decoder=aac  --enable-decoder=ass  --enable-decoder=ssa  --enable-decoder=srt  --enable-decoder=webvtt  --enable-demuxer=matroska \
-    --enable-demuxer=ogg  --enable-demuxer=avi  --enable-demuxer=mov  --enable-demuxer=flv  --enable-demuxer=mpegps  --enable-demuxer=image2 --enable-demuxer=mp3  \
-    --enable-demuxer=concat  --enable-protocol=file --enable-protocol=http --enable-filter=aresample  --enable-filter=scale  --enable-filter=crop  --enable-filter=overlay  --disable-bzlib  --disable-iconv  \
-    --disable-libxcb  --disable-lzma  --disable-sdl  --disable-securetransport  --disable-xlib  --disable-zlib  --enable-encoder=libx264  --enable-encoder=libmp3lame  --enable-encoder=aac  \
-    --enable-muxer=mp4  --enable-muxer=mp3 --enable-muxer=image2 --enable-muxer=null  --enable-gpl  --enable-libmp3lame --enable-libvpx --enable-libx264 --disable-hwaccels --disable-parsers --disable-bsfs --disable-debug --disable-protocols \
-    --disable-indevs --disable-outdevs \
+--disable-all --disable-ffplay --disable-ffprobe --disable-ffserver --disable-asm --disable-doc --disable-devices --disable-pthreads --disable-w32threads --disable-network \
+--disable-asm --disable-fast-unaligned --disable-pthreads --disable-w32threads --disable-os2threads --disable-debug --disable-stripping \
+--disable-network --disable-dxva2 --disable-vaapi --disable-vda --disable-vdpau --disable-bzlib  --disable-iconv --disable-libxcb  --disable-lzma \
+--disable-sdl  --disable-securetransport  --disable-xlib  --disable-zlib --disable-hwaccels --disable-parsers --disable-bsfs --disable-debug \
+--disable-protocols --disable-indevs --disable-outdevs \
+--enable-ffmpeg --enable-avcodec --enable-avcodec  --enable-avformat  --enable-avutil  --enable-swresample  --enable-swscale  --enable-avfilter \
+--enable-decoder=vp8  --enable-decoder=vp9  --enable-decoder=theora  --enable-decoder=mpeg2video  --enable-decoder=mpeg4 \
+--enable-decoder=h264  --enable-decoder=hevc  --enable-decoder=png  --enable-decoder=mjpeg  --enable-decoder=vorbis  --enable-decoder=opus  --enable-decoder=mp3 \
+--enable-decoder=ac3  --enable-decoder=aac  --enable-decoder=ass  --enable-decoder=ssa  --enable-decoder=srt  --enable-decoder=webvtt  --enable-demuxer=matroska \
+--enable-demuxer=ogg  --enable-demuxer=avi  --enable-demuxer=mov  --enable-demuxer=flv  --enable-demuxer=mpegps  --enable-demuxer=image2 --enable-demuxer=mp3  \
+--enable-demuxer=concat  --enable-protocol=file --enable-protocol=http --enable-filter=aresample  --enable-filter=scale  --enable-filter=crop  --enable-filter=overlay \
+--enable-encoder=libx264  --enable-encoder=libmp3lame  --enable-encoder=aac --enable-muxer=mp4  --enable-muxer=mp3 --enable-muxer=image2 --enable-muxer=null \
+--enable-gpl  --enable-libmp3lame --enable-libvpx --enable-libx264
 
 make
 make install
