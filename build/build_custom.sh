27c6e184a7e4717890604667697c33b78b7ad89e
Anuragh27crony <Anuragh27crony@gmail.com>
2017-09-17 19:11:27 +0200

    debug flags

--- build/build_custom.sh
+++ build/build_custom.sh
@@ -13,6 +13,17 @@ emmake make install
 cd ..
 
 
+cd libvpx
+## Try some of these options: ./configure --target=js1-none-clang_emscripten --disable-examples --disable-docs --disable-multithread --disable-runtime-cpu-detect --disable-optimizations --disable-vp8-decoder --disable-vp9-decoder --extra-cflags="-O2"
+make clean
+emconfigure ./configure --prefix=$(pwd)/../dist --disable-examples --disable-docs \
+  --disable-runtime-cpu-detect --disable-multithread --disable-optimizations \
+  --target=generic-gnu
+emmake make
+emmake make install
+cd ..
+
+
 # x264-snapshot-20140501-2245
 cd x264
 make clean
